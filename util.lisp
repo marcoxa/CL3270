0b0e223a7ab79cebf547381dfc90956a32d77aca
Marco Antoniotti <marco.antoniotti@unimib.it>
2025-12-11 23:35:04 +0100

    Added a few utilities to handle dates and dictionaries (hash-tables).

--- util.lisp
+++ util.lisp
@@ -1,4 +1,4 @@
-;;;; -*- Mode: Lisp -*-
+;;;; -*- Mode: Lisp: Encoding UTF-8 -*-
 
 ;;;; util.lisp
 ;;;;
@@ -53,7 +53,7 @@ The value -1 is used to indicate invalid positions."
   )
 
 
-;;; Strings and octets.
+;;; Strings and octects.
 ;;; Just not to depend on BABEL or FLEXI-STREAMS
 
 (defun octets-to-string (octets)
@@ -68,5 +68,72 @@ The value -1 is used to indicate invalid positions."
   (with-output-to-string (result)
     (loop for o across octets
           do (write-char (code-char o) result))))
+
+
+;;; Dictionaries
+;;;
+;;; Useful wrapper around MAKE-HASH-TABLE.
+
+(defun make-dict (&rest keys &key (initial-map ()) &allow-other-keys)
+  (remf keys :initial-map)
+  (loop with ht of-type hash-table = (apply #'make-hash-table keys)
+        for (k v) in initial-map
+        do (setf (gethash k ht) v)
+        finally (return ht)))
+
+
+(defun dict-keys (dict)
+  (declare (type hash-table dict))
+  (loop for k being the hash-key of dict collect k))
+
+
+(defun dict-values (dict)
+  (declare (type hash-table dict))
+  (loop for v being the hash-value of dict collect v))
+
+
+(defun dict-kv-pairs (dict)
+  (declare (type hash-table dict))
+  (loop for k being the hash-key of dict using (hash-value v) collect (cons k v)))
+
+
+;;; Dates
+
+(declaim (ftype (function (&optional boolean boolean boolean) string)
+                today-date
+                now-time))
+
+(defun today-date (&optional h m s)
+  "Return a string representing today's date.
+
+The format of the string is `YYYY-MM-DD` if the optional arguments H,
+M, and S are all NIL.  Otherwise hours, minuts and seconds are added
+to the result, accordingly."
+
+  (declare (type boolean h m s))
+
+  (multiple-value-bind (ss mn hh dd mm yy dw dsp tz)
+      (decode-universal-time (get-universal-time))
+    (declare (type (mod 60) ss mm)
+             (type (mod 24) hh)
+             (type (integer 0) yy)
+             (type (mod 7) dw)
+             (type boolean dsp)
+             (type (rational -24 24) tz))
+    (declare (ignorable ss mm hh dw dsp tz))
+
+    (with-output-to-string (today nil :element-type 'character)
+      (format today "~D-~2,'0D-~2,'0D" yy mm dd)
+      (when h
+        (format today ":~2,'0D" hh)
+        (when m
+          (format today ":~2,'0D" mn)
+          (when s
+            (format today ":~2,'0D" ss)))))))
+
+
+(defun now-time (&optional h m s)
+  (today-date h m s))
+
 
 ;;;; end of file -- util.lisp
