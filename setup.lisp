e3ecda2c09a47bb098223ca0e372feafbc915f43
Marco Antoniotti <marco.antoniotti@unimib.it>
2025-12-14 12:06:50 +0100

    Greatly simplified using David Cooper's suggestion. Cf., SETUP-SOURCE-LOCATION. Most code moved in 'generate.lisp.

--- setup.lisp
+++ setup.lisp
@@ -12,62 +12,25 @@
 (in-package "CL3270")
 
 
-(defparameter *cl3270-source-location*
-  nil
-  "The location (a directory) where the CL3270 source is found.
-
-If NIL, it means that the package has not been properly installed.")
-
-
-(declaim (type (or null pathname) *cl3270-source-location*))
-
-
-;;; Ensuring *cl3270-source-location* has a proper value.
-;;; Repetition and kludginess necessary to handle system definition (mk or asdf)
-;;; or no system definition, in which case we must handle load time vs
-;;; compile time.
+;;; setup-source-pathname
 ;;;
-;;; Using CLAD may ease some of this stuff.
+;;; Thanks to David Cooper for this nifty trick.  It is needed to
+;;; circument COMPILE-FILE output changes, which is what ASDF and MK
+;;; essentially do.
+
+(defmacro setup-source-pathname ()
+  "Substitute the truename of the 'setup.lisp' in the code."
+  `,(or *compile-file-truename* *load-truename*))
 
 
-(eval-when (:load-toplevel :execute)
-  (let ((cl3270-loc (or
-                     #+asdf
-                     (and (asdf:find-system "cl3270" nil)
-                          (asdf:system-source-directory "cl3270"))
+;;; *cl3270-source-location*
+;;;
+;;; Note the use of SETUP-SOURCE-LOCATION.
 
-                     #+mk-defsystem
-                     (and (mk:find-system "cl3270" :load-or-nil)
-                          (mk:system-definition-pathname "cl3270"))
+(defparameter *cl3270-source-location*
+  (make-pathname :name nil :type nil :defaults (setup-source-pathname))
+  "The location (a directory) where the CL3270 source is found.")
 
-                     #-(or asdf mk-defsystem)
-                     *load-pathname*
-                     ))
-        )
-    (unless *cl3270-source-location*
-      (setq *cl3270-source-location*
-            (make-pathname :name nil :type nil
-                           :defaults cl3270-loc)))))
-
-
-(eval-when (:compile-toplevel :execute)
-  (let ((cl3270-loc (or
-                     #+asdf
-                     (and (asdf:find-system "cl3270" nil)
-                          (asdf:system-source-directory "cl3270"))
-
-                     #+mk-defsystem
-                     (and (mk:find-system "cl3270" :load-or-nil)
-                          (mk:system-definition-pathname "cl3270"))
-
-                     #-(or asdf mk-defsystem)
-                     *compile-file-pathname*
-                     ))
-        )
-    (unless *cl3270-source-location*
-      (setq *cl3270-source-location*
-            (make-pathname :name nil :type nil
-                           :defaults cl3270-loc)))))
-
+(declaim (type pathname *cl3270-source-location*))
 
 ;;;; end of file -- setup.lisp
